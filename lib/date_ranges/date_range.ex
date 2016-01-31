926ea4228a9b0c1848ad6ccd82deac2fe49a6b6b
Joseph Kain <joekain@gmail.com>
2016-01-31 14:42:24 -0800

    overlapping/1 function

--- lib/date_ranges/date_range.ex
+++ lib/date_ranges/date_range.ex
@@ -1,10 +1,16 @@
 defmodule DateRanges.DateRange do
   use Ecto.Schema
+  import Ecto.Query, only: [from: 1, from: 2]
 
   schema "date_ranges" do
     field :start, Ecto.Date
     field :end, Ecto.Date
 
     timestamps
+  end
+
+  def overlapping(target) do
+    from range in DateRanges.DateRange, where: fragment("(?, ?) OVERLAPS (?, ?)",
+               range.start, range.end, type(^target.start, Ecto.Date), type(^target.end, Ecto.Date))
   end
 end
