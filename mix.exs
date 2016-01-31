b58a6db96f5c06e9c6b541393200f32c03ed0679
Joseph Kain <joekain@gmail.com>
2016-01-31 08:35:14 -0800

    Install and Start Ecto

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule DateRanges.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :postgrex, :ecto]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,9 @@ defmodule DateRanges.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:postgrex, ">= 0.0.0"},
+      {:ecto, "~> 1.0"},
+    ]
   end
 end
